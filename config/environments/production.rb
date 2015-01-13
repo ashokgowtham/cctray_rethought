dad4acdd81350c81d6d2ee88721d8647052e1de8
Ashok Gowtham M <ashokgowtham123@gmail.com>
2015-01-13 13:22:41 +0530

    Ashok | Remove DB related properties completely

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # No DB # Do not dump schema after migrations.
+  # config.active_record.dump_schema_after_migration = false
 end
